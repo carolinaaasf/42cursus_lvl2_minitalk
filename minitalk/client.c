dbd55c9078443467cb514ed6bad39034f1756ac7
Carolina Silva Figueiredo <csilva-f@c1r7s2.42porto.com>
2022-12-15 23:54:43 +0000

    v0

---
+++ minitalk/client.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csilva-f <csilva-f@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/15 21:21:52 by csilva-f          #+#    #+#             */
+/*   Updated: 2022/12/15 21:49:35 by csilva-f         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/libft.h"
+#include "libftprintf/ft_printf.h"
+#include <signal.h>
+#include <unistd.h>
+
+void	send_signal(int pid, char *str)
+{
+	int	i;
+	int	c;
+
+	i = 0;
+	while (*str)
+	{
+		c = *(str);
+		while (i < 8)
+		{
+			if (c << i & 0b00000000)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			i++;
+			usleep(100);
+		}
+		str++;
+		i = 0;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 3)
+		ft_printf("The client must receive 2 parameters: the server PID and the string to send. Please try again.\n");
+	else
+		send_signal(ft_atoi(argv[1]), argv[2]);
+	return (0);
+}
---
+++ minitalk/server.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csilva-f <csilva-f@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/15 22:40:42 by csilva-f          #+#    #+#             */
+/*   Updated: 2022/12/15 22:52:46 by csilva-f         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftprintf/ft_printf.h"
+#include <signal.h>
+#include <unistd.h>
+
+void	handler(int sig)
+{
+	static int			i = 0;
+	static unsigned int	c = 0;
+
+	if (sig == SIGUSR2)
+		c = c << 1;
+	else if (sig == SIGUSR1)
+		c = (c << 1) | 0b00000001;
+	i++;
+	if (i == 8)
+	{
+		ft_printf("%c", c);
+		i = 0;
+		c = 0;
+	}
+}
+
+int	main(void)
+{
+	ft_printf("Server process id: \n", getpid());
+	while (1)
+	{
+		signal(SIGUSR1, handler);
+		signal(SIGUSR2, handler);
+	}
+	return (0);
+}
